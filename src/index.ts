cf2165c7ca28468e9a27a9acb9849f65ba6224c2
Vadim Budarin <budarin-vv@yandex.ru>
2023-12-18 20:45:00 +0300

    fix export

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from './validateEntity.js';
 
 export * from './isInteger.js';
 export * from './isIntegerInRange.js';
-export * from './isUndefinedOrInteger.js';
 export * from './mustBeInt.js';
 export * from './mustBeUndefinedOrInt.js';
 
@@ -15,7 +14,6 @@ export * from './isISODateTimeString.js';
 export * from './stringHasWrongLength.js';
 
 export * from './isBoolean.js';
-export * from './isUndefinedOrBoolean.js';
 export * from './mustBeUndefinedOrBoolean.js';
 
 export * from './isObject.js';
